217080bec19aa4df45ffb5cca30bb2476007b3ec
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2025-11-17 08:10:50 +0700

    feat: Initialize backend project structure

---
+++ srcCode/BackEnd/src/main/java/com/alcohol/alcoholdetectionsystem/AlcoholDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alcohol.alcoholdetectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholDetectionSystemApplication.class, args);
+	}
+
+}
