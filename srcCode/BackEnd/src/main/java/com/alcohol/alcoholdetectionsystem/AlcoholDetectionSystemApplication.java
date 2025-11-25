19af6a3fe72b794852b1688a379e36c9167213be
Chu Ngọc Thắng <chuthanglsz@gmail.com>
2025-11-25 14:51:59 +0700

    fix: fix env

--- srcCode/BackEnd/src/main/java/com/alcohol/alcoholdetectionsystem/AlcoholDetectionSystemApplication.java
+++ srcCode/BackEnd/src/main/java/com/alcohol/alcoholdetectionsystem/AlcoholDetectionSystemApplication.java
@@ -2,10 +2,23 @@ package com.alcohol.alcoholdetectionsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AlcoholDetectionSystemApplication {
 
+	static {
+		try {
+			Dotenv dotenv = Dotenv.configure()
+					.directory("./srcCode/BackEnd")
+					.ignoreIfMissing()
+					.load();
+			dotenv.entries().forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue()));
+		} catch (Exception e) {
+			System.out.println(".env file not found");
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AlcoholDetectionSystemApplication.class, args);
 	}
